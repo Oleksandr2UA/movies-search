30bb26ae88e04f396e77a502f5b20abdc85a968a
kotik <kotik@alumni.uv.es>
2024-08-15 17:10:28 +0200

    tryy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components/App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { GlobalStyles } from "./components/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/movies-search">
+    <HashRouter basename="/movies-search">
       <GlobalStyles />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
