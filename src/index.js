c799f4fc8fc9022c3705ab0da522df8ca8d55b74
kotik <kotik@alumni.uv.es>
2024-08-15 16:01:05 +0200

    added basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { GlobalStyles } from "./components/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="movies-search">
       <GlobalStyles />
       <App />
     </BrowserRouter>
